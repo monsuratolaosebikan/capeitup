29a1005cef73f2e39998522098236580b4b9fd0f
Monsurat Olaosebikan <monsuratolaosebikan@gmail.com>
2017-06-23 19:01:53 -0400

    added search functionality to request user stats

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+   constructor(props) {
+
+       super(props);
+
+       this.state = { username : ''};
+   }
+
+  handleKeyPress(target) {
+      if(target.charCode === 13){
+          this.search(this.state.username);
+          this.setState({ username: '' });
+      }
+  }
+
+  search(username) {
+     axios.get(`https://apps.runescape.com/runemetrics/profile?user=${encodeURI(username)}&activities=0`)
+         .then(res => {
+             console.log(res.data);
+         })
+  }
+
   render() {
     return (
       <div className="App">
           <h1 className="logo">Cape It Up</h1>
-          <input type="text" spellCheck="false"></input>
+          <input
+              type="text"
+              spellCheck="false"
+              value={this.state.username}
+              onChange={event => this.setState({username: event.target.value})}
+              onKeyPress={this.handleKeyPress.bind(this)}/>
       </div>
     );
   }
